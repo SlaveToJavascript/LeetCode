e169bac2a2d83a8439788b33f08a49e81d137f89
Cindy Zheng <zxx.njc@gmail.com>
2024-05-05 20:58:09 +0800

    updated tags

--- Two Pointers/ShortestWayToFormString.py
+++ Two Pointers/ShortestWayToFormString.py
@@ -1,7 +1,7 @@
 # 1055. Shortest Way to Form String
 # https://leetcode.com/problems/shortest-way-to-form-string/
 # MEDIUM
-# Tags: twopointerslc, #1055
+# Tags: twopointerslc, premiumlc, #1055
 
 # GIVEN:
     # 2 strings, source and target
