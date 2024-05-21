5d6d85f8fb51341287e5daee9e7e21ab34452435
Cindy Zheng <zxx.njc@gmail.com>
2024-05-21 19:52:39 +0800

    updated files

--- Intervals/MeetingRoomsII.py
+++ Intervals/MeetingRoomsII.py
@@ -1,6 +1,7 @@
+# 253. Meeting Rooms II
 # https://leetcode.com/problems/meeting-rooms-ii/
 # MEDIUM
-# Tags: intervalslc, #253
+# Tags: intervalslc, premiumlc, #253
 
 # GIVEN:
     # an array of meeting time intervals, intervals, where intervals[i] = [start_i, end_i]
@@ -33,16 +34,13 @@
 
 def roomsNeeded(intervals):
     max_rooms = 0 # return value
-    starts, ends = [], [] # all start and end times, each sorted
-
-    for s, e in intervals:
-        starts.append(s)
-        ends.append(e)
+    starts = [[i[0] for i in intervals]]
+    ends = [[i[1] for i in intervals]]
     
     starts.sort()
     ends.sort()
     s, e = 0, 0 # s = pointer for starts array, e = pointer for ends array
-    rooms = 0 # track no. of rooms needed at any point in time; this is not max_rooms (max_rooms tracks the max value of rooms)
+    rooms = 0 # track no. of rooms needed at any point in time; this is not max_rooms (max_rooms tracks the max value of "rooms")
 
     while s < len(starts): # s will definitely reach the end before e does since start times are always before end times
         if starts[s] < ends[e]: # if current start time is less than current end time -> new meeting started before previous one ended -> 1 additional room needed for this new meeting
